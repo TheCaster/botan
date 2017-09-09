11b66bcd67761ea56af2cf61276915b340bae147
Jack Lloyd <jack@randombit.net>
2017-09-08 21:11:07 -0400

    Don't need clcache

--- src/scripts/ci/setup_appveyor.bat
+++ src/scripts/ci/setup_appveyor.bat
@@ -10,10 +10,3 @@ cl
 
 appveyor DownloadFile http://download.qt.io/official_releases/jom/jom.zip -FileName jom.zip
 7z e jom.zip
-
-nuget install -ExcludeVersion -Version 4.1.0 clcache
-
-set PATH=%PATH%;C:\projects\botan\clcache\clcache-4.1.0
-
-rem AppVeyor has a hard 1Gb cache limit for OSS builds
-clcache -M 157286400
